7845ff00985782d8181867c725499687ac2c4f31
Djiby223 <113242900+Djiby223@users.noreply.github.com>
2026-07-17 21:44:29 +0000

    Made improvement to the file search.py

---
+++ search.py
@@ -0,0 +1,15 @@
+crop = st.selectbox(
+    t["crop"],
+    [
+        "General",
+        "Rice",
+        "Maize",
+        "Millet",
+        "Sorghum",
+        "Cotton",
+        "Groundnut",
+        "Cowpea",
+        "Sesame",
+        "Vegetables"
+    ]
+)
