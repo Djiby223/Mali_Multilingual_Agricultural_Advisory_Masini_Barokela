eba2099a8ee9899a9b591678c3211478310461fb
Djiby223 <113242900+Djiby223@users.noreply.github.com>
2026-07-22 16:22:04 +0000

    Changed the content of excel_to_json.py

--- scripts/excel_to_json.py
+++ scripts/excel_to_json.py
@@ -1,14 +1,15 @@
-from openpyxl import load_workbook
 from pathlib import Path
+from openpyxl import load_workbook
 import zipfile
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
 EXCEL_FILE = PROJECT_ROOT / "data" / "Test.xlsx"
 
+print("File:", EXCEL_FILE)
 print("Exists:", EXCEL_FILE.exists())
 print("ZIP:", zipfile.is_zipfile(EXCEL_FILE))
 
 wb = load_workbook(EXCEL_FILE)
 
-print("SUCCESS")
-print(wb.sheetnames)
+print("SUCCESS!")
+print("Sheets:", wb.sheetnames)
