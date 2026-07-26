fce614bbf22b68ac3d42c753ab048460c8789d82
Djiby223 <113242900+Djiby223@users.noreply.github.com>
2026-07-26 21:21:42 +0000

    Changes to add Stop Words

--- utils/search.py
+++ utils/search.py
@@ -5,6 +5,27 @@ Multilingual Search Engine
 
 from utils.loader import load_knowledge_base
 
+from utils.loader import load_knowledge_base
+
+# Common words to ignore
+STOP_WORDS = {
+
+    # English
+    "the", "is", "are", "a", "an", "to", "of",
+    "for", "on", "in", "at", "by", "what",
+    "when", "where", "how", "why", "can",
+    "should", "do", "does", "i", "my",
+
+    # French
+    "le", "la", "les", "de", "du", "des",
+    "un", "une", "et", "pour", "dans",
+    "comment", "quand", "où", "est",
+    "je", "mon", "ma", "mes",
+
+    # Bambara
+    "ye", "ka", "ni", "la", "be",
+    "ani", "i", "aw", "n", "o"
+}
 
 def normalize(text):
     """
