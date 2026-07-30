3876c418f572ce598e11d75616055b9e17cd8c78
Djiby223 <113242900+Djiby223@users.noreply.github.com>
2026-07-30 00:51:43 +0000

    Created the save function

--- utils/history.py
+++ utils/history.py
@@ -1,2 +1,21 @@
 import json
 import os
+HISTORY_FILE = "logs/conversations.json"
+
+def save_conversation(chat):
+
+    os.makedirs("logs", exist_ok=True)
+
+    if os.path.exists(HISTORY_FILE):
+
+        with open(HISTORY_FILE, "r", encoding="utf-8") as f:
+            history = json.load(f)
+
+    else:
+
+        history = []
+
+    history.append(chat)
+
+    with open(HISTORY_FILE, "w", encoding="utf-8") as f:
+        json.dump(history, f, ensure_ascii=False, indent=4)
