af85d691528aadf2fce6d5027db4fe0e3213fcf1
Djiby223 <113242900+Djiby223@users.noreply.github.com>
2026-07-30 01:34:07 +0000

    Changed the content of the file history.py

--- utils/history.py
+++ utils/history.py
@@ -1,21 +1,30 @@
 import json
-import os
-HISTORY_FILE = "logs/conversations.json"
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+
+LOGS_DIR = PROJECT_ROOT / "logs"
+
+HISTORY_FILE = LOGS_DIR / "conversations.json"
+
 
 def save_conversation(chat):
 
-    os.makedirs("logs", exist_ok=True)
+    LOGS_DIR.mkdir(exist_ok=True)
 
-    if os.path.exists(HISTORY_FILE):
+    history = []
+
+    if HISTORY_FILE.exists():
 
         with open(HISTORY_FILE, "r", encoding="utf-8") as f:
             history = json.load(f)
 
-    else:
-
-        history = []
-
     history.append(chat)
 
     with open(HISTORY_FILE, "w", encoding="utf-8") as f:
-        json.dump(history, f, ensure_ascii=False, indent=4)
+        json.dump(
+            history,
+            f,
+            indent=4,
+            ensure_ascii=False
+        )
