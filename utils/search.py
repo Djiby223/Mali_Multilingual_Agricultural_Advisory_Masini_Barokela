8898a57017505b0230ed803c4d4fbde84bbb192c
Djiby223 <113242900+Djiby223@users.noreply.github.com>
2026-07-30 21:56:00 +0000

    Added print message to search.py

--- utils/search.py
+++ utils/search.py
@@ -4,6 +4,8 @@ Multilingual Search Engine
 Version 4.1
 """
 
+print(">>> LOADED utils/search.py <<<")
+
 from rapidfuzz import fuzz
 from utils.loader import load_knowledge_base
 
@@ -50,7 +52,7 @@ def search_question(user_question, language):
             best_score = score
             best_record = record
             print(f"WRatio={fuzz.WRatio(user_question, question)}, overlap={overlap}, final score={score}")
-            
+
     if best_score >= MIN_SCORE:
         return best_record, best_score
 
