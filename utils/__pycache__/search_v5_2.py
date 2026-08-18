a9dc59148912b5af923e3f728952f23d591d36d9
Djiby223 <113242900+Djiby223@users.noreply.github.com>
2026-08-18 01:08:08 +0000

    Added intent cues & scoring adjustment

--- utils/__pycache__/search_v5_2.py
+++ utils/__pycache__/search_v5_2.py
@@ -152,6 +152,33 @@ INTENT_CUES = {
     },
 }
 # --------------------------------------------------
+# Intent cues for semantic alignment
+# --------------------------------------------------
+
+INTENT_CUES = {
+
+    "PLANTING_TIME": {
+        "time",
+        "season",
+        "period",
+        "timely",
+    },
+
+    "PLANTING_DEPTH": {
+        "depth",
+        "deep",
+        "shallow",
+    },
+
+    "PLANTING_SPACING": {
+        "spacing",
+        "apart",
+        "distance",
+        "row",
+        "rows",
+    },
+}
+# --------------------------------------------------
 # Stopwords
 # --------------------------------------------------
 
@@ -485,6 +512,42 @@ def search_question_v5_2(
             + (token_similarity * 0.25)
             + (coverage * 0.30)
         )
+
+        # --------------------------------------------------
+# Intent alignment
+# --------------------------------------------------
+
+intent_cues = INTENT_CUES.get(
+    sub_intent,
+    set(),
+)
+
+candidate_cues = set()
+
+for cue_intent, cues in INTENT_CUES.items():
+
+    if cue_intent == sub_intent:
+        continue
+
+    if meaningful_tokens(
+        question_normalized,
+        language_key,
+    ) & cues:
+
+        candidate_cues.add(cue_intent)
+
+if (
+    meaningful_tokens(
+        question_normalized,
+        language_key,
+    ) & intent_cues
+):
+
+    score += 15
+
+elif candidate_cues:
+
+    score -= 15
 	# --------------------------------------------------
 # Intent alignment
 # --------------------------------------------------
